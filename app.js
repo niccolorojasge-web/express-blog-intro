63f26cfa824c67ca1945876f4563b73084d70b2b
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-02-05 15:25:54 +0100

    server del mio blog

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+    res.send("<h1>server del mio blog</h1>")
+})
+app.listen(port, () => {
+    console.log(`example app listening on port ${port}`)
+})
